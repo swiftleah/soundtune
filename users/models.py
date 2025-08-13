6a6cf8cd69c3153404222da78dd0645b4794cee3
swiftleah <leahrosehenry@gmail.com>
2025-08-13 14:59:29 +0200

    Added default fields to is_staff, is_active for CustomUser + migrated. User creation working.

--- users/models.py
+++ users/models.py
@@ -28,8 +28,8 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractUser, PermissionsMixin):
     full_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    is_active = models.BooleanField(True)
-    is_staff = models.BooleanField(False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email' #email instead of username
